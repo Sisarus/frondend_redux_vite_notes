cf961a792dbc5858174f325bcce2b96949e72ac9
Alisa <alisa.luoma@hotmail.fi>
2023-10-12 15:51:49 +0300

    Adding show data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Notes from "./components/Notes"
+import NewNote from "./components/NewNote"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+  return(
+    <div>
+      <NewNote />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />
+          All
+        </label>
+
+        <label>
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />
+          Important
+        </label>
+
+        <label>
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />
+          Non-Important
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Notes />
+    </div>
   )
 }
 
